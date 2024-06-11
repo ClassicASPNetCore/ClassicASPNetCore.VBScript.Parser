5103be974c03836af9d4636d33e0de97e5d7f254
Oscar Rovira <560186+krosk93@users.noreply.github.com>
2024-06-11 18:00:18 +0200

    Initial commit

---
+++ src/ClassicASPNetCore.VBScript.Parser/Library.fs
@@ -0,0 +1,82 @@
+﻿namespace ClassicASPNetCore.VBScript.Parser
+open FParsec
+
+type Value =
+    | Empty
+    | Null
+    | Boolean of bool
+    | Byte of byte
+    | Integer of int
+    | Currency of decimal
+    | Long of int64
+    | Single of single
+    | Double of double
+    | Date of string
+    | String of string
+    | Object of obj
+    | Error of int
+
+type Operator =
+    | Exponentiation
+    | UnaryNegation
+    | Multiplication
+    | Division
+    | IntegerDivision
+    | Modulus
+    | Addition
+    | Subtraction
+    | StringConcatenation
+    | Equality
+    | Inequality
+    | LessThan
+    | GreaterThan
+    | LessOrEqualThan
+    | GreaterOrEqualThan
+    | ObjectEquivalence
+    | LogicalNegation
+    | LogicalConjunction
+    | LogicalDisjunction
+    | LogicalExclusion
+    | LogicalEquivalence
+    | LogicalImplication
+
+type Expr =
+    | Literal of Value
+    | Variable of name:string
+    | Operation of (Expr * Operator * Expr)
+
+type Statement =
+    | Set of name:string * value:Expr
+    | If of condition:Expr * body:Block * Else:Block option
+    | While of condition:Expr * body:Block
+and Block = Statement list
+
+
+module VBScriptParser =
+
+    let private pword s = pstring s .>> spaces
+
+    let private parens p = between (pword "(") (pword ")") p
+
+    let private pbool =
+        pword "True" <|> pword "False"
+        |>> function
+            | "True" -> Boolean (true)
+            | "False" -> Boolean (false)
+
+    let private pint = pint32 |>> int |>> Integer
+
+    let private pstringliteral =
+        pchar '\"' >>. manyCharsTill anyChar (pchar '\"')
+        |>> string |>> String
+        .>> spaces
+
+    let private pblocks = choice [
+        pBlock
+        pInstruction
+    ]
+
+    let parseString text =
+        match runParserOnString (many pblocks) () "" text with
+        | Success (result, _, _) -> List.toSeq result
+        | Failure (error, _, _) -> raise (System.Exception(error))
